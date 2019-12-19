f8c919fef7dd7b8f39830c0d2c24440d154d700f
caismith-ch <csmith2@companieshouse.gov.uk>
2019-12-19 14:40:43 +0000

    Update method of calling post to search api to follow example of extensions

--- src/controllers/search.controller.ts
+++ src/controllers/search.controller.ts
@@ -1,25 +1,79 @@
 import {Request, Response} from "express";
-import axios, { AxiosResponse, AxiosError } from "axios";
+import axios, { AxiosResponse, AxiosError, Method, AxiosRequestConfig } from "axios";
 
 const url: string = 'http://localhost:4089/alphabetical-search/corporate-name'
 
-export default (req: Request, res: Response) => {
-    let companyName: string = req.query["company-name"];
+const HTTP_POST: Method = "post";
 
-    axios({
-        method: 'post',
-        url: url,
-        data : {
-            company_name: companyName
-        },
-        headers : {
-            'Content-Type': 'application/json'
+export interface CompaniesResource {
+    searchType: string,
+    topHit: string,
+    results: [
+        {
+            ID: string,
+            company_type: string,
+            items: {
+                company_number: string,
+                company_status: string,
+                corporate_name: string,
+                record_type: string
+            },
+            links: {
+                self: string
+            }
         }
-    })
-    .then((axiosResponse: AxiosResponse) => {
-        res.send(axiosResponse.data);
-    })
-    .catch((axiosError: AxiosError) => {
-        console.log(axiosError);
-    });
+    ]
+}
+
+const getBaseAxiosRequestConfig = (): AxiosRequestConfig => {
+    return {
+        headers: {
+            Accept: "application/json"
+        }
+    };
+};
+
+const makeAPICall = async (config: AxiosRequestConfig): Promise<AxiosResponse> => {
+    try {
+        return await axios.request<any>(config);
+    } catch (err) {
+        console.log(`API ERROR ${err}`);
+        const axiosError = err as AxiosError;
+        const{response, message} = axiosError;
+        throw {
+            data: response ? response.data.errors : [],
+            message,
+            status: response ? response.status : -1,
+        };
+    }
+};
+
+const getApiData = async (config: AxiosRequestConfig): Promise<any> => {
+    const axiosResponse: AxiosResponse = await makeAPICall(config);
+    const data = axiosResponse.data;
+    console.log(`data returned from axios api call : ${data}`);
+    return data;
+}
+
+const getCompanies = async (companyName: string): Promise<CompaniesResource> => {
+    const GET_COMPANIES_PATH = "http://localhost:4089/alphabetical-search/corporate-name";
+
+    const config: AxiosRequestConfig = getBaseAxiosRequestConfig();
+    config.headers["Content-Type"] = "application/json";
+    config.data = {
+        company_name: companyName
+    }
+    config.method = HTTP_POST;
+    config.url = GET_COMPANIES_PATH;
+
+    const data = await getApiData(config) as CompaniesResource;
+    return data;
+}
+
+export default async (req: Request, res: Response) => {
+    let companyName: string = req.query["company-name"];
+    
+    const response = await getCompanies(companyName);
+
+    res.send(response.results);
 };
