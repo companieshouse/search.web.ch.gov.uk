e418b0f8985ef90c1c435178c40ee288d875ecce
caismith-ch <csmith2@companieshouse.gov.uk>
2020-01-13 16:21:48 +0000

    Prepare code base for deployment using concourse

--- src/app.ts
+++ src/app.ts
@@ -27,9 +27,13 @@ app.set("view engine", "html");
 env.addGlobal("PIWIK_URL", "https://example.com");
 env.addGlobal("PIWIK_SITE_ID", "123");
 
-// serve static assets in development. this will not execute in production.
-if (process.env.NODE_ENV === "development") {
+// serve static assets in development.
+// this will execute in production for now, but we will host these else where in the future.
+if (process.env.NODE_ENV !== "production") {
   app.use("/alphabetical-search/static", express.static("dist/static"));
+  env.addGlobal("CSS_URL", "/alphabetical-search/static/app.css");
+} else {
+  app.use("/alphabetical-search/static", express.static("static"));
   env.addGlobal("CSS_URL", "/alphabetical-search/static/app.css");
 }
 // apply our default router to /
