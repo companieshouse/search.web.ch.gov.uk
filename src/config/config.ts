2a25e925ad54307b76cc56705ec0dd8da9ef45a0
caismith-ch <csmith2@companieshouse.gov.uk>
2020-01-14 12:59:36 +0000

    Extract api endpoint into an environment variable

---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+const getEnvironmentValue = (key: string, defaultValue?: any): string => {
+    const isMandatory: boolean = !defaultValue;
+    const value: string = process.env[key] || "";
+
+    if (!value && isMandatory) {
+        throw new Error(`Please set the environment variable "${key}"`);
+    }
+
+    return value || defaultValue as string;
+};
+
+export const ALPHABETICAL_SEARCH_URL = getEnvironmentValue("ALPHABETICAL_SEARCH_URL");
