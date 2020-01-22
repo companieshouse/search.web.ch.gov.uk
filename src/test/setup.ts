18acc9fbc5eeb4f78a28a0dd672d0790bd2d2a68
caismith-ch <csmith2@companieshouse.gov.uk>
2020-01-22 11:51:34 +0000

    Add missing env variable to test setup causing pipeline to fail at build-unit-test stage

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,5 @@
 export default async () => {
   process.env.ALPHABETICAL_SEARCH_URL = "http://localhost:4089/alphabetical-search/corporate-body";
+  process.env.AUTH_KEY = "testauthkey";
   process.env.SEARCH_WEB_COOKIE_NAME = "search.web.user";
 }
