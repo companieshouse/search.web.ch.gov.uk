67fdb1fca4f8dabf65a0a82f48bc8c2dcb68bad7
ethatcher83 <ethatcher@companieshouse.gov.uk>
2021-01-25 13:04:49 +0000

    refactored logging to use ch-structured-logging module

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -4,9 +4,13 @@
  * Module dependencies.
  */
 
-import * as http from "http";
-import * as yargs from "yargs";
+import http from "http";
+import yargs from "yargs";
 import app from "../app";
+import { createLogger } from "ch-structured-logging";
+import { APPLICATION_NAME } from "../config/config";
+
+const logger = createLogger(APPLICATION_NAME);
 
 /**
  * Get port from environment and store in Express.
@@ -66,12 +70,12 @@ function onError(error) {
   switch (error.code) {
     case "EACCES":
       // tslint:disable-next-line
-      console.error(bind + " requires elevated privileges");
+      logger.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
     case "EADDRINUSE":
       // tslint:disable-next-line
-      console.error(bind + " is already in use");
+      logger.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -89,5 +93,5 @@ function onListening() {
     ? "pipe " + addr
     : "port " + addr!.port;
   // tslint:disable-next-line
-  console.log("Listening on " + bind);
+  logger.info("Listening on " + bind);
 }
