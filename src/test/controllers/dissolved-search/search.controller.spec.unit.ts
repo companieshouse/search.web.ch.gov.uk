493fc023aff98f72dc3805d9f3927e80a2bcab6a
katherine-axten <kaxten@companieshouse.gov.uk>
2021-03-01 15:28:09 +0000

    Fixed tests to work with the new sdk dates

--- src/test/controllers/dissolved-search/search.controller.spec.unit.ts
+++ src/test/controllers/dissolved-search/search.controller.spec.unit.ts
@@ -7,6 +7,9 @@ import { formatPostCode, formatDate, sanitiseCompanyName } from "../../../../src
 const sandbox = sinon.createSandbox();
 let testApp = null;
 let getCompanyItemStub;
+const cessation_date = new Date(1991, 11, 12);
+const creation_date = new Date(1991, 11, 12);
+const emptyDate = new Date();
 
 const mockResponseBody : CompaniesResource = ({
     etag: "etag",
@@ -19,13 +22,13 @@ const mockResponseBody : CompaniesResource = ({
             company_name: "test company",
             company_number: "0000789",
             company_status: "dissolved",
-            date_of_cessation: "19910212",
-            date_of_creation: "19910212",
+            date_of_cessation: cessation_date,
+            date_of_creation: creation_date,
             kind: "kind",
             previous_company_names: [
                 {
-                    ceased_on: "19910212",
-                    effective_from: "19910212",
+                    ceased_on: cessation_date,
+                    effective_from: creation_date,
                     name: "old name"
                 }
             ]
@@ -40,13 +43,13 @@ const mockResponseBody : CompaniesResource = ({
         company_name: "test company",
         company_number: "0000789",
         company_status: "active",
-        date_of_cessation: "19910212",
-        date_of_creation: "19910212",
+        date_of_cessation: cessation_date,
+        date_of_creation: creation_date,
         kind: "kind",
         previous_company_names: [
             {
-                ceased_on: "19910212",
-                effective_from: "19910212",
+                ceased_on: cessation_date,
+                effective_from: creation_date,
                 name: "old name"
             }
         ]
@@ -65,13 +68,13 @@ const emptyMockResponseBody : CompaniesResource = ({
         company_name: "",
         company_number: "",
         company_status: "",
-        date_of_cessation: "",
-        date_of_creation: "",
+        date_of_cessation: emptyDate,
+        date_of_creation: emptyDate,
         kind: "kind",
         previous_company_names: [
             {
-                ceased_on: "",
-                effective_from: "",
+                ceased_on: emptyDate,
+                effective_from: emptyDate,
                 name: ""
             }
         ]
@@ -118,17 +121,25 @@ describe("search.controller.spec.unit", () => {
         });
     });
 
+    describe("check it returns a dissolved results page with a shortened postcode successfully when the postcode has a space in an odd place space", () => {
+        it("should return a results page with a shortened postcode successfully", () => {
+            const postCode: string = "CF56R B";
+
+            chai.expect(formatPostCode(postCode)).to.contain("CF56");
+        });
+    });
+
     describe("check it returns a dissolved results page with a the date in DD Jan YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19900125";
+            const newDate = "1990-01-25"
 
-            chai.expect(formatDate(date)).to.contain("25 Jan 1990");
+            chai.expect(formatDate(newDate)).to.contain("25 Jan 1990");
         });
     });
 
     describe("check it returns a dissolved results page with a the date in DD Feb YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19900225";
+            const date = "1990-02-25";
 
             chai.expect(formatDate(date)).to.contain("25 Feb 1990");
         });
@@ -136,7 +147,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD Mar YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19900325";
+            const date = "1990-03-25";
 
             chai.expect(formatDate(date)).to.contain("25 Mar 1990");
         });
@@ -144,7 +155,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD Apr YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19900425";
+            const date = "1990-04-25";
 
             chai.expect(formatDate(date)).to.contain("25 Apr 1990");
         });
@@ -152,7 +163,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD May YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19900525";
+            const date= "1990-05-25";
 
             chai.expect(formatDate(date)).to.contain("25 May 1990");
         });
@@ -160,7 +171,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD Jun YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19900625";
+            const date= "1990-06-25";
 
             chai.expect(formatDate(date)).to.contain("25 Jun 1990");
         });
@@ -168,7 +179,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD Jul YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19900725";
+            const date= "1990-07-25";
 
             chai.expect(formatDate(date)).to.contain("25 Jul 1990");
         });
@@ -176,7 +187,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD Aug YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19900825";
+            const date = "1990-08-25";
 
             chai.expect(formatDate(date)).to.contain("25 Aug 1990");
         });
@@ -184,7 +195,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD Sep YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19900925";
+            const date = "1990-09-25";
 
             chai.expect(formatDate(date)).to.contain("25 Sep 1990");
         });
@@ -192,7 +203,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD Oct YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19901025";
+            const date = "1990-10-25";
 
             chai.expect(formatDate(date)).to.contain("25 Oct 1990");
         });
@@ -200,7 +211,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD Nov YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19941125";
+            const date = "1994-11-25";
 
             chai.expect(formatDate(date)).to.contain("25 Nov 1994");
         });
@@ -208,7 +219,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD Dec YYYY format for both columns", () => {
         it("should return a results page with a correct date format", () => {
-            const date: string = "19901225";
+            const date = "1990-12-25";
 
             chai.expect(formatDate(date)).to.contain("25 Dec 1990");
         });
@@ -216,7 +227,7 @@ describe("search.controller.spec.unit", () => {
 
     describe("check it returns a dissolved results page with a the date in DD MM YYYY format for both columns", () => {
         it("should return a results page with a correct date format if it can't workout the month", () => {
-            const date: string = "19900025";
+            const date: string = "1990-00-25";
 
             chai.expect(formatDate(date)).to.contain("25 00 1990");
         });
