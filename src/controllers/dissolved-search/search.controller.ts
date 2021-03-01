b10d823d366e7ce27a24e6813b7db30b130e7c1a
katherine-axten <kaxten@companieshouse.gov.uk>
2021-03-01 16:52:18 +0000

    Split the formatting functions into a utils file

--- src/controllers/dissolved-search/search.controller.ts
+++ src/controllers/dissolved-search/search.controller.ts
@@ -4,9 +4,9 @@ import { createGovUkErrorData, GovUkErrorData } from "../../model/govuk.error.da
 import { CompaniesResource } from "@companieshouse/api-sdk-node/dist/services/search/dissolved-search/types";
 import { createLogger } from "@companieshouse/structured-logging-node";
 import { getDissolvedCompanies } from "../../client/apiclient";
-import escape from "escape-html";
 
 import { SEARCH_WEB_COOKIE_NAME, API_KEY, APPLICATION_NAME, LAST_UPDATED_MESSAGE } from "../../config/config";
+import { formatDate, formatPostCode, sanitiseCompanyName } from "../utils";
 import * as templatePaths from "../../model/template.paths";
 import * as errorMessages from "../../model/error.messages";
 import Cookies = require("cookies");
@@ -70,78 +70,6 @@ const route = async (req: Request, res: Response) => {
             templateName: templatePaths.NO_RESULTS
         });
     }
-};
-
-export const sanitiseCompanyName = (companyName) => {
-    return escape(companyName);
-};
-
-export const formatPostCode = (postCode) => {
-    let halfPostCode;
-    let trimmedPostCode;
-
-    if (postCode != null) {
-        const newPostCode = postCode.split(" ");
-        halfPostCode = newPostCode[0];
-
-        trimmedPostCode = halfPostCode.slice(0, 4);
-    }
-
-    return trimmedPostCode;
-};
-
-export const formatDate = (unformattedDate) => {
-    const date = unformattedDate.toString();
-    const splitDate = date.split("-");
-    const year = splitDate[0];
-    const month = splitDate[1];
-    const day = splitDate[2];
-    let monthWord;
-
-    switch (month) {
-    case "01":
-        monthWord = "Jan";
-        break;
-    case "02":
-        monthWord = "Feb";
-        break;
-    case "03":
-        monthWord = "Mar";
-        break;
-    case "04":
-        monthWord = "Apr";
-        break;
-    case "05":
-        monthWord = "May";
-        break;
-    case "06":
-        monthWord = "Jun";
-        break;
-    case "07":
-        monthWord = "Jul";
-        break;
-    case "08":
-        monthWord = "Aug";
-        break;
-    case "09":
-        monthWord = "Sep";
-        break;
-    case "10":
-        monthWord = "Oct";
-        break;
-    case "11":
-        monthWord = "Nov";
-        break;
-    case "12":
-        monthWord = "Dec";
-        break;
-    default:
-        monthWord = month;
-    }
-
-    const dateToReturn = [day, monthWord, year].join(" ");
-
-    return dateToReturn;
 };
 
 export default [...validators, route];
