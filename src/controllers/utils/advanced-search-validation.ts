98802c7c29ce9f53014b3ad3354ce38e5a5ea52c
hwilliams1 <hwilliams1@companieshouse.gov.uk>
2021-10-28 11:18:48 +0100

    Initial poc attempt for testing to and from dates

--- src/controllers/utils/advanced-search-validation.ts
+++ src/controllers/utils/advanced-search-validation.ts
@@ -6,6 +6,7 @@ const INCORPORATED_FROM_FIELD: string = "incorporatedFrom";
 const INCORPORATED_TO_FIELD: string = "incorporatedTo";
 
 const INVALID_DATE_ERROR_MESSAGE = "You must enter a valid date, for example, 01/01/2009";
+const TO_DATE_BEFORE_FROM_DATE = "The incorported from date should be earlier than the incorporated to date";
 
 export const advancedSearchValidationRules =
     [
@@ -28,6 +29,24 @@ export const advancedSearchValidationRules =
                     }
                 }
                 return true;
+            }),
+        check(INCORPORATED_FROM_FIELD)
+            .custom(async (from, {req}) => {
+                let fromDate = new Date(from);
+                let toDate = new Date(req.query?.incorporatedTo);
+                console.log(`fromDate = ${fromDate} and toDate = ${toDate}`)
+                if (toDate < fromDate) {
+                    throw Error(TO_DATE_BEFORE_FROM_DATE);
+                }
+            }),
+        check(INCORPORATED_TO_FIELD)
+            .custom(async (to, {req}) => {
+                let toDate = new Date(to);
+                let fromDate = new Date(req.query?.incorporatedFrom);
+                console.log(`fromDate = ${fromDate} and toDate = ${toDate}`)
+                if (toDate < fromDate) {
+                    throw Error(TO_DATE_BEFORE_FROM_DATE);
+                }
             })
     ];
 
