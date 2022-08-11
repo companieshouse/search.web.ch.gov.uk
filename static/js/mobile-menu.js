8667b5a069df4f71987403cfaf952d0fb352d0d7
ethatcher83 <ethatcher@companieshouse.gov.uk>
2022-08-11 09:21:07 +0100

    rename mobile menu file change to use query selector

--- static/js/mobile-menu.js
+++ static/js/mobile-menu.js
@@ -1,8 +1,8 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById("ul#navigation").classList.add("mobile-hidden");
+    document.querySelector("ul#navigation").classList.add("mobile-hidden");
     document.querySelector(".js-toggle-nav .js-header-toggle").click(() => {
         event.preventDefault();
-        document.getElementById("ul#navigation").classList.toggle("mobile-hidden");
+        document.querySelector("ul#navigation").classList.toggle("mobile-hidden");
         document.querySelector("#global-nav").classList.toggle("open");
     })
 })
