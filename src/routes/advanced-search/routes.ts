d0aa435fabaca5120ef6add6be292ccc70c0fa13
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-11-01 11:39:59 +0000

    BI-11902: Add basket link to advanced search sign in/sign out bar.

--- src/routes/advanced-search/routes.ts
+++ src/routes/advanced-search/routes.ts
@@ -1,9 +1,10 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { SEARCH_WEB_COOKIE_NAME } from "../../config/config";
 import { searchController } from "../../controllers/advanced-search/index.controller";
 import downloadController from "../../controllers/advanced-search/download.controller";
 import * as pageUrls from "../../model/page.urls";
 import * as templatePaths from "../../model/template.paths";
+import { BasketLink, getBasketLink } from "../../controllers/utils/utils";
 import uuid = require("uuid/v4");
 import Cookies = require("cookies");
 
@@ -14,14 +15,19 @@ const router = Router();
  *ß
  * @param template the template name
  */
-const renderTemplate = (template: string) => (req: Request, res: Response) => {
-    const cookies = new Cookies(req, res);
+const renderTemplate = (template: string) => async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const cookies = new Cookies(req, res);
+        const basketLink: BasketLink = await getBasketLink(req);
 
-    if (cookies === undefined || cookies.get(SEARCH_WEB_COOKIE_NAME) === undefined) {
-        cookies.set(SEARCH_WEB_COOKIE_NAME, uuid());
+        if (cookies === undefined || cookies.get(SEARCH_WEB_COOKIE_NAME) === undefined) {
+            cookies.set(SEARCH_WEB_COOKIE_NAME, uuid());
+        }
+
+        return res.render(template, basketLink);
+    } catch (error) {
+        next(error);
     }
-
-    return res.render(template);
 };
 
 router.get(pageUrls.ADVANCED_ROOT, renderTemplate(templatePaths.ADVANCED_INDEX));
