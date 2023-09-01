57492d293a6ca14d44f643d0bfd3298d328f0869
mcolwellch <mcolwell@companieshouse.gov.uk>
2023-09-01 09:15:56 +0100

    locals.tf edit

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -8,7 +8,7 @@ locals {
   lb_listener_rule_priority = 11
   lb_listener_paths         = [
     "/*search",
-    "/search*/",
+    "/*search",
     "/search-web/health"
   ]
   healthcheck_path          = "/search-web/health" # healthcheck path for search web
@@ -19,18 +19,19 @@ locals {
   vpc_name = local.service_secrets["vpc_name"]
 
   parameter_store_secrets = {
-    "account_url"         = local.service_secrets["account_url"]
-    "api_url"             = local.service_secrets["api_url"]
-    "cache_server"        = local.service_secrets["cache_server"]
-    "cdn_host"            = local.service_secrets["cdn_host"]
-    "chs_api_key"         = local.service_secrets["chs_api_key"]
-    "chs_monitor_gui_url" = local.service_secrets["chs_monitor_gui_url"]
-    "chs_url"             = local.service_secrets["chs_url"]
-    "cookie_domain"       = local.service_secrets["cookie_domain"]
-    "cookie_secret"       = local.service_secrets["cookie_secret"]
-    "piwik_site_id"       = local.service_secrets["piwik_site_id"]
-    "piwik_url"           = local.service_secrets["piwik_url"]
-    "vpc_name"            = local.service_secrets["vpc_name"]
+    "account_url"               = local.service_secrets["account_url"]
+    "alphabetical_search_url"   = local.service_secrets["alphabetical_search_url"]
+    "api_url"                   = local.service_secrets["api_url"]
+    "cache_server"              = local.service_secrets["cache_server"]
+    "cdn_host"                  = local.service_secrets["cdn_host"]
+    "chs_api_key"               = local.service_secrets["chs_api_key"]
+    "chs_monitor_gui_url"       = local.service_secrets["chs_monitor_gui_url"]
+    "chs_url"                   = local.service_secrets["chs_url"]
+    "cookie_domain"             = local.service_secrets["cookie_domain"]
+    "cookie_secret"             = local.service_secrets["cookie_secret"]
+    "piwik_site_id"             = local.service_secrets["piwik_site_id"]
+    "piwik_url"                 = local.service_secrets["piwik_url"]
+    "vpc_name"                  = local.service_secrets["vpc_name"]
   }
 
   # create a map of secret name => secret arn to pass into ecs service module
