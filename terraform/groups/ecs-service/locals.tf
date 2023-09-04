89264950ad7e381df69c475a5282507bd7a9791a
mcolwellch <mcolwell@companieshouse.gov.uk>
2023-09-04 12:04:48 +0100

    Saved a file...

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -1,9 +1,9 @@
 # Define all hardcoded local variable and local variables looked up from data resources
 locals {
-  stack_name                = "search-service" # this must match the stack name the service deploys into
+  stack_name                = "search-service"  # this must match the stack name the service deploys into
   name_prefix               = "${local.stack_name}-${var.environment}"
-  service_name              = "search-web" # testing service name
-  container_port            = "3000"                    # default node port required here until prod docker container is built allowing port change via env var
+  service_name              = "search-web"      # testing service name
+  container_port            = "3000"            # default node port required here until prod docker container is built allowing port change via env var
   docker_repo               = "search.web.ch.gov.uk"
   lb_listener_rule_priority = 11
   lb_listener_paths         = [
