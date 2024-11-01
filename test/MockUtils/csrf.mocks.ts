1fa2004aa0255bcb046d96c0df1461d3f5456cbf
rmcmurray-ch <rmcmurray@companieshouse.gov.uk>
2024-11-01 12:02:03 +0000

    path update

--- test/MockUtils/csrf.mocks.ts
+++ test/MockUtils/csrf.mocks.ts
@@ -14,7 +14,7 @@ export const getAppWithMockedCsrf = (sandbox: sinon.SinonSandbox) => {
     };
 
     // Return the app with the stubbed WebSecurity module using proxyquire
-    return proxyquire("../../../src/app", {
+    return proxyquire("../../src/app", {
         "@companieshouse/web-security-node": stubbedWebSecurity
     }).default;
 };
