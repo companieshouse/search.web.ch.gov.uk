a1b1c5e0533199bdc11ad635e4279d12cb94a7fc
rmcmurray-ch <rmcmurray@companieshouse.gov.uk>
2024-11-05 12:56:09 +0000

    error controller and mocks file

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -1,11 +1,31 @@
 import { NextFunction, Request, Response } from "express";
 import * as errorMessages from "../model/error.messages";
 import * as templatePaths from "../model/template.paths";
-import { APPLICATION_NAME } from "../config/config";
+import { APPLICATION_NAME, CHS_URL } from "../config/config";
 import { createLogger } from "@companieshouse/structured-logging-node";
+import { PageHeader } from "../model/PageHeader";
+import { mapPageHeader } from "../utils/page.header.utils";
+import { CsrfError } from '@companieshouse/web-security-node';
 
 const logger = createLogger(APPLICATION_NAME);
+const SERVICE_URL = CHS_URL;
 
+/**
+ * This handler catches CSRF errors when a CRSF attack is detected.
+ */
+const csrfErrorHandler = (err: CsrfError | Error, req: Request, res: Response, next: NextFunction) => {
+    // Handle non-CSRF Errors immediately
+    if (!(err instanceof CsrfError)) {
+      return next(err);
+    }
+
+    const pageHeader: PageHeader = mapPageHeader(req);
+    return res.status(403).render(templatePaths.ERROR, {
+        errorMessage: err,
+        ...pageHeader,
+        SERVICE_URL
+    });
+};
 /**
  * This handler catches all routes that don't match a handler i.e. 404 Not Found, because of its position
  * in the middleware chain.
