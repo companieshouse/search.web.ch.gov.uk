7533b90b288bed29c564dc47c57321c6364bbc63
ch-ncolley <ncolley@companieshouse.gov.uk>
2025-08-27 09:49:58 +0100

    Fix code lint errors

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 const getEnvironmentValue = (key: string, defaultValue?: any): string => {
-    const isMandatory: boolean = typeof defaultValue === undefined;
+    const isMandatory: boolean = typeof defaultValue === "undefined";
     const value: string = process.env[key] || "";
 
     if (!value && isMandatory) {
